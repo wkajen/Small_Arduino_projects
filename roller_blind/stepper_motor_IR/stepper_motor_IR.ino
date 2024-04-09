7365f7de4f07d92857f0532b98c1524a269f5b80
Kajetan Walawski <kajetan7777-2000@tlen.pl>
2024-04-09 18:22:04 +0200

    Upgrade
    Big upgrade from initial versions to final version of 3D parts. Some changes in code (added automatic work).

--- roller_blind/stepper_motor_IR/stepper_motor_IR.ino
+++ roller_blind/stepper_motor_IR/stepper_motor_IR.ino
@@ -1,11 +1,12 @@
 // made by Kajetan Walawski
 #include <IRremote.h>
-#include "arduino-timer.h"
 
-// all connected pins
+#define FOTORES_PIN A5
+#define FOTORES_THR_NIGHT 960
+#define FOTORES_THR_DAY 770
+
+// all digital PINS plugged in
 const int irPin = 2; 
-const int leftBtn = 6;
-const int rightBtn = 7;
 const int motorPin1 = 8;
 const int motorPin2 = 9;
 const int motorPin3 = 10;
@@ -13,32 +14,38 @@ const int motorPin4 = 11;
 const int greenLedPin = 12;
 const int redLedPin = 13;
 
-// decoded hex IR values
-const int leftArrow = 0xC8EE0693;
-const int rightArrow = 0xDD332AF;
-const int upArrow = 0x2F7D0DEF;
-const int downArrow = 0x47E58AD3;
-const int okKey = 0x1CEDF82B;
-const int playKey = 0x6220794F;
-const int backKey = 0xA80FD90F;
-const int menuKey = 0x20624873;
+// recorded values of signals from the IR pilot
+const int leftArrow = 0xFF22DD;  //0xC8EE0693;
+const int rightArrow = 0xFF02FD; //0xDD332AF;
+const int upArrow = 0xFFA857;    //0x2F7D0DEF;
+const int downArrow = 0xFFE01F;  //0x47E58AD3;
+const int zeroKey = 0xFF6897;
+const int oneKey = 0xFF30CF;
+const int twoKey = 0xFF18E7;
+const int threeKey = 0xFF7A85;
+const int infoKey = 0xFF906F;
+const int stopKey = 0xFFC23D; 
 
 IRrecv irrecv(irPin);
-decode_results irSignal;
+decode_results results;
 
-auto timer = timer_create_default(); // create a timer with default settings
-
+// global variables
 int stepNo = 0;
-boolean spinCW = false;
-boolean spinCCW = false;
+int stepNoTotal = 0;
+int stepsLimit = 25000;
+int receivedVal = 0;
+bool spinCW = false;
+bool spinCCW = false;
+bool isUp = false;
+bool isDown = false;
+bool isAutoWork = false;
 boolean redLedOn = false;
 boolean greenLedOn = false;
 
 void setup() {
-  irrecv.enableIRIn(); //enable receiving IR data
+  // Serial.begin(9600); //UART - for debbuging
+  irrecv.enableIRIn(); //enable receiving data
 
-  pinMode(leftBtn, INPUT_PULLUP);
-  pinMode(rightBtn, INPUT_PULLUP);
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
   pinMode(motorPin3, OUTPUT);
@@ -54,57 +61,94 @@ void loop() {
   // function calls
   IRSignalFun();
   MotorSpinning();
-  LEDlights();
 
-  // tick the timer
-  timer.tick(); 
+  if(isAutoWork) {
+    AutoWork();
+    LedLight(false, false);
+  } 
+  else {
+    LedLight(false, true);
+  }
 }
 
+
 void IRSignalFun(){
-  if (irrecv.decode(&irSignal)) {
-    int receivedVal = irSignal.value;
+  if (irrecv.decode(&results)) {  // when IR signal received
+    receivedVal = results.value;
     switch(receivedVal){
-      case leftArrow:   // turn clockwise
+      case leftArrow:   // spin clockwise
         spinCW = true; 
         spinCCW = false;
         break;
-      case rightArrow:  // turn counter clockwise
+      case rightArrow:  // spin counter clockwise
         spinCW = false;
         spinCCW = true;
         break;
-      case backKey:      // turn off motor spinning
+      case stopKey:      // turn off motor spinning
         spinCW = false;
         spinCCW = false;
-        // timer.cancel();
         break;
-      case upArrow:
-        redLedOn ^= true;
+      /* //just for debbuging
+      case infoKey:
+        Serial.print("Light = ");
+        Serial.print(analogRead(FOTORES_PIN));
+        Serial.print(" ; isUp = ");
+        Serial.print(isUp);
+        Serial.print(" ; isDown = ");
+        Serial.print(isDown);
+        Serial.print(" ; AUTO = ");
+        Serial.print(isAutoWork); 
+        Serial.print(" ; StepLimit = ");
+        Serial.print(stepsLimit); 
+        Serial.print(" ; Step = ");
+        Serial.println(stepNoTotal);
         break;
-      case downArrow:
-        greenLedOn ^= true;
+      */
+      case zeroKey:                  // set UP position
+        isUp = true;
+        isDown = false;
+        LedLight(true, false);       // visualize the change
+        delay(250);
+        LedLight(false, false);
+        break;
+      case oneKey:                   // set DOWN position
+        isUp = false;
+        isDown = true;
+        LedLight(true, false);       // visualize the change
+        delay(250);
+        LedLight(false, false);
+        break;
+      case twoKey:                   // toggle auto work
+        isAutoWork ^= true;
+        stepNoTotal = 0;
+        break;  
+      case threeKey:                 // option to change threshold
+        stepsLimit = stepNoTotal;
+        LedLight(true, false);
+        delay(500);
+        LedLight(false, false);
         break;
     }
-    irrecv.resume();  // wait for new IR signal
+    irrecv.resume(); // Receive the next value
   }
 }
 
 void MotorSpinning(){
-  if(spinCW == true || digitalRead(leftBtn) == LOW) {           //left button pushed down
-    OneStepMotor(true);                                         //spin CW
+  if(spinCW) { 
+    OneStepMotor(1); 
     delay(2);
-    timer.every(1000, ToggleLed);
   }
-  else if(spinCCW == true || digitalRead(rightBtn) == LOW) {    //right button pushed down
-    OneStepMotor(false);                                        //spin CCW
+  else if(spinCCW) { 
+    OneStepMotor(2);
     delay(2);
-    timer.every(1000, ToggleLed);
   }
   else {
-    timer.cancel();   // turn off all active timer actions 
+    OneStepMotor(0);  // stop spinning
+    // redLedOn = false;
   }
 }
 
-void LEDlights(){
+void LedLight(bool redLedOn, bool greenLedOn){
   if(redLedOn == true) { 
     digitalWrite(redLedPin, HIGH);
   } else {
@@ -117,13 +161,8 @@ void LEDlights(){
   }
 }
 
-bool ToggleLed() {
-  redLedOn ^= true; // toggle the LED
-  return true;      // keep timer active? true
-}
-
-void OneStepMotor(bool dir){
-  if(dir){
+void OneStepMotor(byte dir){
+  if(dir == 1){
     switch(stepNo){
       case 0:
         digitalWrite(motorPin1, HIGH);
@@ -150,7 +189,7 @@ void OneStepMotor(bool dir){
         digitalWrite(motorPin4, HIGH);
         break;
     } 
-  } else{
+  } else if(dir == 2) {
     switch(stepNo){
       case 0:
         digitalWrite(motorPin1, LOW);
@@ -177,10 +216,47 @@ void OneStepMotor(bool dir){
         digitalWrite(motorPin4, LOW);
         break;
     } 
+  } else {  //stop spinning
+      digitalWrite(motorPin1, LOW);
+      digitalWrite(motorPin2, LOW);
+      digitalWrite(motorPin3, LOW);
+      digitalWrite(motorPin4, LOW);
   }
   
-  stepNo++;
-  if(stepNo > 3){
-    stepNo = 0;
+  if(dir == 1 || dir == 2){
+    stepNo++;
+    stepNoTotal++;
+    if(stepNo > 3){
+      stepNo = 0;
+    }
+  }
+}
+
+void AutoWork() {
+  int fotoResVal = analogRead(FOTORES_PIN);
+
+  if(fotoResVal > FOTORES_THR_NIGHT && isUp == true) {
+    spinCW = true; 
+    spinCCW = false;
+  } 
+  if(fotoResVal < FOTORES_THR_DAY && isDown == true) {
+    spinCW = false; 
+    spinCCW = true; 
+  }
+  if(isUp == true && stepNoTotal > stepsLimit) {
+    isUp = false;
+    spinCW = false; 
+    spinCCW = false;
+    stepNoTotal = 0;
+    delay(500);
+    isDown = true;
+  }
+  if(isDown == true && stepNoTotal > stepsLimit) {
+    isDown = false;
+    spinCW = false; 
+    spinCCW = false;
+    stepNoTotal = 0;
+    delay(500);
+    isUp = true;
   }
 }
